5423dcad5a46424ed3beaf3668e89d75f930b4f6
Sasha <html63@mail.ru>
2022-02-24 07:10:08 +0300

    Small fixes

--- 4.Linux/1_3_2.sh
+++ 4.Linux/1_3_2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 USER_AGENT='Mozilla/5.0 (X11; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0'
-$(wget -U ${USER_AGENT} -q -O /dev/null https://passport.yandex.ru/auth --post-data='login=TestAccountSasha&passwd=3310732357as' --save-cookies=COOKIES_AUTH)
+LOGIN="TestAccountSasha"
+PASS="3310732357as"
+$(wget -U ${USER_AGENT} -q -O /dev/null https://passport.yandex.ru/auth --post-data="login=${LOGIN}&passwd=${PASS}" --save-cookies=COOKIES_AUTH)
 COUNT=$(wget -U ${USER_AGENT} -q -O - https://mail.yandex.ru/ --load-cookie=COOKIES_AUTH --save-cookies=COOKIES.txt | grep -E -o '<span class="b-messages__firstline">[^<]*<\/span>' | wc -l)
 rm -r COOKIES_AUTH
 echo "Number of messages in inbox: ${COUNT}"
